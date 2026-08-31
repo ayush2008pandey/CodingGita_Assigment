5b21d64347293dbc9a7fa26d06b1bc254bcb11e4
ayush2008pandey <ayush2008p@gmail.com>
2026-09-01 00:48:08 +0530

    updatted

---
+++ PYTHON/02/_data_types_practice_problems.py
@@ -0,0 +1,95 @@
+x = 25
+print(type(x))
+#answer - integer
+
+price = 99.50
+print(type(price))
+#answer - float
+
+name = "Python"
+print(type(name))
+#answer - string
+
+is_active = True
+print(type(is_active))
+#answer - bool
+
+data = None
+print(type(data))
+#answer - Nonetype
+
+
+age = 25
+
+
+temperature = 98.6
+
+name = "Alex"
+
+
+is_student = True
+
+
+complex_number = 3 + 5j
+
+
+print(variable := age, type(age))
+print(temperature, type(temperature))
+print(name, type(name))
+print(is_student, type(is_student))
+print(complex_number, type(complex_number))
+
+
+
+student_name = "ayush"
+student_age = 18
+student_height = 5.10
+whether_the_person_is_a_student = True
+student_result = False
+print(student_name,student_age,student_height,whether_the_person_is_a_student,student_result)
+
+
+
+a = 50
+b = 50.0
+c = "50"
+print(a,b,c)
+
+
+
+a = True
+b = "True"
+print(type(a) ,type(b))
+
+
+a = None
+b = "None"
+print(type(a),type(b))
+
+value= 12
+type(value)
+value = "ayush"
+type(value)
+
+print(type(value))
+#answer - the class of value changes from integers to string 
+
+
+product_name=input("enter product name")
+product_quantity = int(input("enter no "))
+product_price= float(input("enter price "))
+
+print(product_name,product_quantity,product_price)
+
+
+print(type(10))
+print(type(10.0))
+print(type("10"))
+
+print(type(True))
+print(type("True"))
+print(type(None))
+print(type("None"))
+
+
+
