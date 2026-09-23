dcbe2e3403fbecc42b88d50242031cdefbf53fa5
ayush2008pandey <ayush2008p@gmail.com>
2026-09-23 09:37:10 +0530

    updated

--- PYTHON/03/topic04.py
+++ PYTHON/03/topic04.py
@@ -1,48 +1,48 @@
-# # question - 12
-# ASCII = input("enter ASCII ")
-# print("unicode of the charachter", ord(ASCII))
+# question - 12
+ASCII = input("enter ASCII ")
+print("unicode of the charachter", ord(ASCII))
 
-# # question - 13
-# unicode = int(input("enter code"))
-# print("ASCII CHARACHTER", chr(unicode))
+# question - 13
+unicode = int(input("enter code"))
+print("ASCII CHARACHTER", chr(unicode))
 
-# # question - 14
-# ascii_A = ord("A")
-# ascii_a = ord("a")
-# print(ascii_A)
-# print(ascii_a)
-# print("a is the larger ")
-
-
-# ascii_B = ord("B")
-# ascii_b = ord("b")
-# print(ascii_B)
-# print(ascii_b)
-# print( "they are not same ")
+# question - 14
+ascii_A = ord("A")
+ascii_a = ord("a")
+print(ascii_A)
+print(ascii_a)
+print("a is the larger ")
 
 
-# # question - 15
-# char = input("enter char ")
-# print("unicode of the char", ord(char))
-
-# # question - 16
-# letter = input("enter letter: ")
-# next_letter = ord(letter)+ 1
-# next_letter = chr(next_letter)
-# print("next letter", next_letter)
+ascii_B = ord("B")
+ascii_b = ord("b")
+print(ascii_B)
+print(ascii_b)
+print( "they are not same ")
 
 
+# question - 15
+char = input("enter char ")
+print("unicode of the char", ord(char))
 
-# # question - 17
+# question - 16
+letter = input("enter letter: ")
+next_letter = ord(letter)+ 1
+next_letter = chr(next_letter)
+print("next letter", next_letter)
 
-# print("A" < "B")
-# #output - ture
-# print("a" < "b")
-# #output - Ture
-# print("A" < "a")
-# #output - True
-# print("0" < "9")
-# #output - True
+
+
+# question - 17
+
+print("A" < "B")
+#output - ture
+print("a" < "b")
+#output - Ture
+print("A" < "a")
+#output - True
+print("0" < "9")
+#output - True
 
 #question - 18
 
